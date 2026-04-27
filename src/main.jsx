6053083b09ac7e881e4bbee78b0467326f4e5444
Carlos Filipe Mello de Figueiredo <filipe@MBP-de-Carlos.home>
2026-04-27 15:45:34 +0100

    chore: force update with version tag v4.0

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
 
+console.log('--- POSEIDON SITE VERSION 4.0 ---');
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 );
